a3dd2c58f3f42513384e2d55ba1e087b51ec85ed
alexHooper <alexmohack@gmail.com>
2018-08-17 21:29:58 +0530

    import our module for sending email with python
    create a connection to gmail and start a secure conn
    
    login into the gmail service with username and password
    
    correct error by changing port to password
    
    send email from username to emails in to_list
    
    quit the connection with server at end
    
    import SMTP class from module
    
    use smtplib exceptions for authentication and sending emails

--- send_email.py
+++ send_email.py
@@ -1,4 +1,27 @@
+from smtplib import SMTP, SMTPAuthenticationError, SMTPException
+
 host = "smtp.gmail.com"
 port = 587
 username = "78030psg@gmail.com"
 password = "thisisauselessemail"
+
+from_email = username
+to_list = [username]
+
+email_conn = SMTP(host, port)
+email_conn.ehlo()
+email_conn.starttls()
+
+try:
+	email_conn.login(username, password)
+except SMTPAuthenticationError as e:
+	print('Failed to login')
+	print(e)
+
+try:
+	email_conn.sendmail(from_email, to_list, "Hello there, we are from python")
+except SMTPException as e:
+	print('Failed to send email')
+	print(e)
+
+email_conn.quit()
