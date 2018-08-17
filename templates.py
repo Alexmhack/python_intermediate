78b5f6ad20d1dda0c66a22af1dda98e4e0e12488
alexHooper <alexmohack@gmail.com>
2018-08-17 23:30:09 +0530

    raise exception if the path does not exist

--- templates.py
+++ templates.py
@@ -2,4 +2,6 @@ import os
 
 def get_template_path(path):
 	file_path = os.path.join(os.getcwd(), path)
+	if os.path.isfile(file_path):
+		raise Exception("This is not a valid template path...")
 	return file_path
