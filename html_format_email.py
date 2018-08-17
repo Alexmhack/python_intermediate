c6584803e8d27da308eb4b6983bc3566e18488c8
alexHooper <alexmohack@gmail.com>
2018-08-17 23:04:44 +0530

    wrap everything in try and except block

--- html_format_email.py
+++ html_format_email.py
@@ -10,30 +10,35 @@ password = "thisisauselessemail"
 from_email = username
 to_list = [username]
 
-email_conn = SMTP(host, port)
-email_conn.ehlo()
-email_conn.starttls()
+try:
+	email_conn = SMTP(host, port)
+	email_conn.ehlo()
+	email_conn.starttls()
 
-message = MIMEMultipart("alternative")
-message['Subject'] = "Hello there"
-message['From'] = from_email
-message['To'] = to_list[0]
+	message = MIMEMultipart("alternative")
+	message['Subject'] = "Hello there"
+	message['From'] = from_email
+	message['To'] = to_list[0]
 
-plain_text = "Testing plain message"
-html_text = """
-<html>
-	<head></head>
-	<body>
-		<h2>Hello Sir</h2>
-		<p>This is me, <a href="https://python.org">python!</a></p>
-	</body>
-</html>
-"""
+	plain_text = "Testing plain message"
+	html_text = """
+	<html>
+		<head></head>
+		<body>
+			<h2>Hello Sir</h2>
+			<p>This is me, <a href="https://python.org">python!</a></p>
+		</body>
+	</html>
+	"""
 
-part_1 = MIMEText(plain_text, 'plain')
-part_2 = MIMEText(html_text, 'html')
+	part_1 = MIMEText(plain_text, 'plain')
+	part_2 = MIMEText(html_text, 'html')
 
-message.attach(part_1)
-message.attach(part_2)
+	message.attach(part_1)
+	message.attach(part_2)
 
-print(message.as_string())
+	email_conn.login(username, password)
+	email_conn.sendmail(from_email, to_list, message.as_string())
+	email_conn.quit()
+except SMTPException as e:
+	print(e)
