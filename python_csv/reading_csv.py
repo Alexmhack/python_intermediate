be073fb4c4e8b566a8806b99f86fafac1709157a
alexHooper <alexmohack@gmail.com>
2018-08-19 12:30:37 +0530

    read data with DictReader

--- python_csv/reading_csv.py
+++ python_csv/reading_csv.py
@@ -4,3 +4,9 @@ with open('data.csv', 'r') as csvfile:
 	reader = csv.reader(csvfile)
 	for row in reader:
 		print(row)
+
+
+with open('data.csv', 'r') as csvfile:
+	reader = csv.DictReader(csvfile)
+	for row in reader:
+		print(row)
