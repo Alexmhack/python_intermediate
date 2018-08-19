93a0ed9072f8d2d5c88385de9109f727b85cdd36
alexHooper <alexmohack@gmail.com>
2018-08-19 12:36:26 +0530

    append csv data using dict keys and DictWriter

--- python_csv/append_csv.py
+++ python_csv/append_csv.py
@@ -4,3 +4,8 @@ with open('data.csv', 'a') as csvfile:
 	writer = csv.writer(csvfile)
 	writer.writerow(['Append row', 'append description', 'append summary'])
 	
+
+with open('data.csv', 'a') as csvfile:
+	fieldnames = ['Title', 'Description', 'Summary']
+	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+	writer.writerow({'Title': 'Row3', 'Description': 'Row3', 'Summary': 'Row3'})
