1ec8e30baf4f62df0326fd1329f3140098b5b1b2
alexHooper <alexmohack@gmail.com>
2018-08-27 18:40:47 +0530

    create a flight model for storing flight details

--- python_flask/models.py
+++ python_flask/models.py
@@ -1,1 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Flight(db.Model):
+	__tablename__ = "flights"
+	id = db.Column(db.Integer, primary_key=True)
+	origin = db.Column(db.String, nullable=False)
+	destination = db.Column(db.String, nullable=False)
+	duration = db.Column(db.Integer, nullable=False)
