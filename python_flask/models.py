f5337b9a64611f39eb4b94f164f6c62fae801c43
alexHooper <alexmohack@gmail.com>
2018-08-27 18:51:33 +0530

    create model for passengers

--- python_flask/models.py
+++ python_flask/models.py
@@ -8,3 +8,9 @@ class Flight(db.Model):
 	origin = db.Column(db.String, nullable=False)
 	destination = db.Column(db.String, nullable=False)
 	duration = db.Column(db.Integer, nullable=False)
+
+class Passenger(db.Model):
+	__tablename__ = "passengers"
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String, nullable=False)
+	flight_id = db.Column(db.Integer, db.ForeignKey("flights.id"), nullable=False)
