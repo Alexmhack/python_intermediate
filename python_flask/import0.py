325eaf59740370d16c33ae50eda98946359083f8
alexHooper <alexmohack@gmail.com>
2018-08-28 18:50:53 +0530

    create file for inserting data from csv file

---
+++ python_flask/import0.py
@@ -0,0 +1,25 @@
+import os
+import csv
+
+from flask import Flask, render_template, request
+from models import *
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+def main():
+	f = open("flights.csv")
+	reader = csv.reader(f)
+
+	for origin, destination, duration in reader:
+		flight = Flight(origin=origin, destination=destination, duration=duration)
+		db.session.add(flight)
+		print(f"ADDED FLIGHT FROM {origin} TO {destination} lasting {duration}")
+	db.session.commit()
+
+
+if __name__ == "__main__":
+	with app.app_context():
+		main()
