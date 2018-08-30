a0dd400d968c2c6e615074802a34ee12bd5cf6f8
alexHooper <alexmohack@gmail.com>
2018-08-30 10:54:40 +0530

    write rows in the file

--- python_csv/append_function.py
+++ python_csv/append_function.py
@@ -7,6 +7,11 @@ def get_length(file_path):
 def append_data(file_path, name, email):
 	fieldnames = ['id', 'name', 'email']
 	next_id = get_length(file_path)
-	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-	writer.writeheader()
-	
+	with open('data.csv', 'a') as csvfile:
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		writer.writeheader()
+		writer.writerow({
+			"id": next_id,
+			"name": name,
+			"email": email
+		})
