daadfe0ddd89b47fa8bb171fcf1ee5a679fc70a3
alexHooper <alexmohack@gmail.com>
2018-08-30 10:52:06 +0530

    add sample method for getting last row

--- python_csv/append_function.py
+++ python_csv/append_function.py
@@ -1,6 +1,12 @@
 import csv
 
+# number of rows
+def get_length(file_path):
+	return 1
+
 def append_data(file_path, name, email):
 	fieldnames = ['id', 'name', 'email']
+	next_id = get_length(file_path)
 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 	writer.writeheader()
+	
