c7d19ab5c645ce39c870a0f219d8a2f10976f058
alexHooper <alexmohack@gmail.com>
2018-09-01 21:32:35 +0530

    add logic to read and write in temp file

--- python_csv/editing_csv_data.py
+++ python_csv/editing_csv_data.py
@@ -2,3 +2,21 @@ import csv
 import shutil
 
 from tempfile import NamedTemporaryFile
+
+FILE_NAME = 'data.csv'
+temp_file = NamedTemporaryFile(delete=False)
+
+with open(FILE_NAME, 'wb', , newline='') as csvfile, temp_file:
+	reader = csv.DictReader(csvfile)
+	fieldnames = ['id', 'name', 'email', 'amount', '']
+	writer = csv.DictWriter(temp_file, fieldnames=fieldnames)
+	writer.writeheader()
+
+	for row in reader:
+		writer.writerow({
+			"id": row["id"],
+			"name": row["name"],
+			"email": row["email"],
+			"amount": "599.99",
+			"sent": ""
+		})
