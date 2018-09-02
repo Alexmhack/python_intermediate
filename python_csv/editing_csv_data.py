2f57c5fda92b8ee8a32e03c62ad32194c8692de8
alexHooper <alexmohack@gmail.com>
2018-09-02 10:33:27 +0530

    logic failed for csv and temp file

--- python_csv/editing_csv_data.py
+++ python_csv/editing_csv_data.py
@@ -4,19 +4,19 @@ import shutil
 from tempfile import NamedTemporaryFile
 
 FILE_NAME = 'data.csv'
-temp_file = NamedTemporaryFile(delete=False)
+temp_file = NamedTemporaryFile(delete=True)
 
-with open(FILE_NAME, 'wb', , newline='') as csvfile, temp_file:
+with open(FILE_NAME, 'rb') as csvfile, temp_file:
 	reader = csv.DictReader(csvfile)
-	fieldnames = ['id', 'name', 'email', 'amount', '']
-	writer = csv.DictWriter(temp_file, fieldnames=fieldnames)
-	writer.writeheader()
+	fieldnames = [b'id', b'name', b'email', b'amount', b'sent']
+	writer = csv.writer(csvfile)
+	# writer.writeheader()
+	print(temp_file.name)
 
-	for row in reader:
-		writer.writerow({
-			"id": row["id"],
-			"name": row["name"],
-			"email": row["email"],
-			"amount": "599.99",
-			"sent": ""
-		})
+	name = b"NAME"
+	temp_file.write(name)
+
+	temp_file.seek(0)
+	print(temp_file.read())
+
+	shutil.move(temp_file.name, FILE_NAME)
