13cf986a2ea7a3aa8534835f668d614af98b588a
alexHooper <alexmohack@gmail.com>
2018-09-02 20:48:32 +0530

    make minor change in description

--- integrate/__main__.py
+++ integrate/__main__.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
 
 parser = ArgumentParser(prog="integrate", usage="%(prog)s [options]",
-	description="Run python codes for handling csv data using the command line commands and options")
+	description="Run python codes for handling csv data using the commands and options")
 
 parser.add_argument("--user_id", type=int, help="enter the user's id for displaying user details")
 
