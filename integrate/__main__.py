ed123bb73649bf88ad97c9dd6f5ad78c0753c101
alexHooper <alexmohack@gmail.com>
2018-09-02 20:39:23 +0530

    add --user_id argument with no logic

--- integrate/__main__.py
+++ integrate/__main__.py
@@ -1,1 +1,10 @@
-print("HELLO RISHABH")
+from argparse import ArgumentParser
+
+parser = ArgumentParser(prog="integrate", usage="%(prog)s [options]",
+	description="Run python codes for handling csv data using the command line commands and options")
+
+parser.add_argument("--user_id", type=int, help="enter the user's id for displaying user details")
+
+args = parser.parse_args()
+
+print(args.user_id)
