ec5ab8060adaefe3435b99d909f853c7437a4b62
alexHooper <alexmohack@gmail.com>
2018-09-02 20:53:45 +0530

    import and use find_user to print returned data

--- integrate/__main__.py
+++ integrate/__main__.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser
 
+from data_manager import find_user
+
 parser = ArgumentParser(prog="integrate", usage="%(prog)s [options]",
 	description="Run python codes for handling csv data using the commands and options")
 
@@ -8,3 +10,4 @@ parser.add_argument("--user_id", type=int, help="enter the user's id for display
 args = parser.parse_args()
 
 print(args.user_id)
+print(find_user(user_id=args.user_id))
